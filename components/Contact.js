b3be066eb50e929717002b3f74e279b20a55c4e0
8danield <69875004+8danield@users.noreply.github.com>
2020-09-25 17:54:43 -0700

    Add features & let's keep in touch (for landing page)
    Import react slideshow, add features & contact js/css, pics for 3 features

---
+++ components/Contact.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import styles from "../styles/Contact.module.css";
+
+const spanStyles = {
+    color: "#80FFDB"
+}
+
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: '' };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ value: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert('An email was submitted: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className={styles.contactBody}>
+                <h1>Let's <span style={spanStyles}>keep in touch</span></h1>
+                <p>Tell us your <strong>email</strong> and we’ll keep you up to date<br></br>with the latest additions to Covalent.</p>
+                <form className={styles.emailForm} onSubmit={this.handleSubmit}>
+                    <label>
+                        <input
+                            className={styles.emailInput}
+                            placeholder="Email"
+                            type="email"
+                            value={this.state.value}
+                            onChange={this.handleChange}
+                        />
+                    </label>
+                    <input
+                        className={styles.emailSubmit}
+                        type="submit"
+                        value="&#10140;"
+                    />
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Contact;
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import { Amplify } from "aws-amplify";
 Amplify.configure({ ssr: true });
 
 import '../styles/globals.css'
+import "react-slideshow-image/dist/styles.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
