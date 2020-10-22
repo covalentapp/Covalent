72daf916efbf2f473183806f2e064971596cddf2
mykull <69874869+mykulls@users.noreply.github.com>
2020-10-21 22:15:40 -0700

    hostmenu routing fixed
    changed host to a component to accomodate; method is a little hacky but it works for now; also added back button compatability

--- pages/menu.js
+++ pages/menu.js
@@ -3,39 +3,63 @@ import Head from "next/head";
 import styles from "../styles/Menu.module.css";
 import SimpleButton from "../components/SimpleButton";
 import routingWrapper from "../components/routingWrapper";
+import HostMenu from "../components/host";
 
 //This file is for the Game Selection/Menu/Homepage for Covalent
 //@Michael
+
+const origin = (process.env.NODE_ENV == 'production') ? "https://covalent.app" : "http://localhost:3000";
 
 class Menu extends Component {
     constructor(props) {
         super(props);
         this.state = {
             code: "",
-            error: false
+            error: false,
+            host: false,
+        };
+    }
+    
+    handleRouteChange = () => {
+        if(window.location.href == origin + "/menu/?host") {
+            this.setState({ host: true });
         }
+    }
+        
+    componentDidMount() {
+        this.props.router.events.on('routeChangeComplete', this.handleRouteChange);
+    }
+
+    componentWillUnmount() {
+        this.props.router.events.off('routeChangeComplete', this.handleRouteChange);
     }
 
     handleJoinClick = (event) => {
-        if (this.state.code != "" && (this.state.code.length == 6)) {
+        if (this.state.code != "" && this.state.code.length == 6) {
             this.props.router.push("join/" + this.state.code);
         } else {
-            this.setState({code: this.state.code, error: true});
+            this.setState({ code: this.state.code, error: true });
         }
     };
 
+
     handleHostClick = (e) => {
         e.preventDefault();
-        this.props.router.push("menu/host");
+        this.props.router.push("/menu//?host", undefined, { shallow: true });
+        this.setState({ host: true });
     };
 
+    hostClose = () => {
+        this.setState({ host: false });
+    }
+
     handleChange = (event) => {
-        this.setState({code: event.target.value, error: false});
+        this.setState({ code: event.target.value, error: false });
     };
 
     render() {
         return (
-            <div className={styles.Menu}>
+            <div className={styles.MenuContainer}>
                 <Head>
                     <meta charSet="utf-8" />
                     <meta
@@ -47,7 +71,10 @@ class Menu extends Component {
                         name="description"
                         content="Remote team-building made super simple"
                     />
-                    <link rel="apple-touch-icon" href="/images/logo192.png" />
+                    <link
+                        rel="apple-touch-icon"
+                        href="/images/logo192.png"
+                    />
                     <link rel="manifest" href="/manifest.json" />
                     <link rel="icon" href="/favicon.ico" />
                     <title>Covalent</title>
@@ -60,46 +87,54 @@ class Menu extends Component {
                         overflow: hidden;
                     }
                 `}</style>
-                <div className={styles.MenuBody}>
-                    {/*<div className={styles.white}>*/}
-                    <img
-                        src="/images/logo.svg"
-                        className={styles.logoImg}
-                        alt="Covalent Logo"
-                    ></img>
-                    <p className={styles.logoText}>COVALENT</p>
-                    {/*</div> div no longer needed without join button animation*/}
-                    <div className={styles.MenuButtons}>
-                        <div className={styles.joinOptions}>
-                            <label className={styles.joinCodeLabel}>
-                            CODE:
-                            <input
-                                placeholder="ABCDEF"
-                                className={styles.codeInput + " " + (this.state.error && styles.codeInputError)}
-                                type="text"
-                                maxLength="6"
-                                value={this.state.code}
-                                onChange={this.handleChange}
-                            />
-                        </label>
-                        </div>
-                        <div className={styles.defaultButton}>
-                            <SimpleButton
-                                name="JOIN"
-                                type="join"
-                                onClick={this.handleJoinClick}
-                            ></SimpleButton>
-                        </div>
-                        <div className={styles.defaultButton}>
-                            <SimpleButton
-                                name="HOST"
-                                type="host"
-                                fontmin="0px"
-                                onClick={this.handleHostClick}
-                            ></SimpleButton>
+                <div className={styles.Menu}>
+                    <div className={styles.MenuBody}>
+                        {/*<div className={styles.white}>*/}
+                        <img
+                            src="/images/logo.svg"
+                            className={styles.logoImg}
+                            alt="Covalent Logo"
+                        ></img>
+                        <p className={styles.logoText}>COVALENT</p>
+                        {/*</div> div no longer needed without join button animation*/}
+                        <div className={styles.MenuButtons}>
+                            <div className={styles.joinOptions}>
+                                <label className={styles.joinCodeLabel}>
+                                    CODE:
+                                    <input
+                                        placeholder="ABCDEF"
+                                        className={
+                                            styles.codeInput +
+                                            " " +
+                                            (this.state.error &&
+                                                styles.codeInputError)
+                                        }
+                                        type="text"
+                                        maxLength="6"
+                                        value={this.state.code}
+                                        onChange={this.handleChange}
+                                    />
+                                </label>
+                            </div>
+                            <div className={styles.defaultButton}>
+                                <SimpleButton
+                                    name="JOIN"
+                                    type="join"
+                                    onClick={this.handleJoinClick}
+                                ></SimpleButton>
+                            </div>
+                            <div className={styles.defaultButton}>
+                                <SimpleButton
+                                    name="HOST"
+                                    type="host"
+                                    fontmin="0px"
+                                    onClick={this.handleHostClick}
+                                ></SimpleButton>
+                            </div>
                         </div>
                     </div>
                 </div>
+                {this.state.host ? <HostMenu close={this.hostClose}/> : null}
             </div>
         );
     }
