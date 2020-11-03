2a72705753693a90b37c560cea46efe1f1856f2a
mykull <69874869+mykulls@users.noreply.github.com>
2020-11-03 02:25:37 -0800

    fixed navbar error
    broken code that i accidentally committed sometime earlier

--- components/landing/NavBar.js
+++ components/landing/NavBar.js
@@ -33,8 +33,6 @@ export default function NavBar() {
             } else {
                 setHeight(8.25);
             }
-
-            h = document.getElementsByClassName(styles.nav).style.height;
         }
 
         window.addEventListener("resize", handleResize);
