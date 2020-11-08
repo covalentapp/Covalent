463d5a641ece8943db97a21e1590860b9e25b661
mykull <69874869+mykulls@users.noreply.github.com>
2020-11-08 14:57:09 -0800

    insta link + footer formatting

--- components/landing/Footer.js
+++ components/landing/Footer.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import styles from '../../styles/landing/Footer.module.css';
+import React from "react";
+import styles from "../../styles/landing/Footer.module.css";
 
-
-export default function Footer(){
-
-        return (
-            <div className={styles.footer}>
-                <p className={styles.content}>
-                    <span id={styles.alignright}><a href="https://twitter.com/covalentapp">Follow us on Twitter!</a></span>
-                    <span>&#169; Covalent 2020</span>
-                </p>
-            </div>
-        );
-
+export default function Footer() {
+    return (
+        <div className={styles.footer}>
+                <span id={styles.alignright}>
+                    <div className={styles.twitter}>
+                    <a href="https://twitter.com/covalentapp">
+                        Follow us on Twitter!
+                    </a>
+                    </div>
+                    <div>
+                    <a href="https://instagram.com/covalentapp?igshid=1xjtnl953g3e6">
+                        Follow us on Instagram!
+                    </a>
+                    </div>
+                </span>
+                <span className={styles.copyright}>&#169; Covalent 2020</span>
+        </div>
+    );
 }
