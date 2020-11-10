086d3a16379dd46e5b497bd5a090cbf1c7327d07
muthu-pal <muthupal@g.ucla.edu>
2020-11-09 22:09:53 -0800

    deleted <Head> from error components

--- components/ErrorFullGame.js
+++ components/ErrorFullGame.js
@@ -7,25 +7,6 @@ class ErrorNotFound extends Component {
     render () {
         return (
             <div className={styles.ErrorNotFound}>
-                <Head>
-                    <meta charSet="utf-8" />
-                    <meta
-                        name="viewport"
-                        content="width=device-width, initial-scale=1"
-                    />
-                    <meta name="theme-color" content="#000000" />
-                    <meta
-                        name="description"
-                        content="Remote team-building made super simple"
-                    />
-                    <link
-                        rel="apple-touch-icon"
-                        href="/images/logo192.png"
-                    />
-                    <link rel="manifest" href="/manifest.json" />
-                    <link rel="icon" href="/favicon.ico" />
-                    <title>Covalent</title>
-                </Head>
                 <style jsx global>{`
                     img {
                         width: 411.08px;
