1ad066e8fb99bcbd7aa87c9d4c46791113025e19
Daniel Inhee Lee <lee.danny112358@gmail.com>
2020-11-24 21:04:56 -0800

    added pointer-events styling to video to fix playback bug

--- components/VideoLib.js
+++ components/VideoLib.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
-import VideoRecorder from 'react-video-recorder'
-import LoadingView from './VideoRecorder/loading-view.js'
-import UnsupportedView from './VideoRecorder/unsupported-view.js'
-import ErrorView from './VideoRecorder/error-view.js'
-import styles from '../styles/Video.module.css'
+import React, { Component } from "react";
+import VideoRecorder from "react-video-recorder";
+import LoadingView from "./VideoRecorder/loading-view.js";
+import UnsupportedView from "./VideoRecorder/unsupported-view.js";
+import ErrorView from "./VideoRecorder/error-view.js";
+import styles from "../styles/Video.module.css";
 
 /*
 Known issues:
@@ -12,48 +12,41 @@ Known issues:
 */
 
 class GameVideoRecorder extends Component {
-    render() {
-        return (
-            <div className={styles.recorder}>
-                <VideoRecorder
-                    isOnInitially={true}
-                    timeLimit={10000}
-                    onRecordingComplete={this.props.onRecordingComplete}
-                    renderLoadingView={() => <LoadingView />}
-                    renderUnsupportedView={() => <UnsupportedView />}
-                    renderErrorView={() => <ErrorView />}
-                    /*
-                    The setting below enables replay controls, but there's some issues with the overlay on chrome.
-                    Need to do customizations before enabling
-                    showReplayControls={true}
-                    */
-                />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className={styles.recorder}>
+        <VideoRecorder
+          isOnInitially={true}
+          timeLimit={10000}
+          onRecordingComplete={this.props.onRecordingComplete}
+          renderLoadingView={() => <LoadingView />}
+          renderUnsupportedView={() => <UnsupportedView />}
+          renderErrorView={() => <ErrorView />}
+          showReplayControls={true}
+        />
+      </div>
+    );
+  }
 }
 
 class VideoPlayback extends Component {
-
-    render() {
-
-        return (
-            <div>
-                {this.props.video &&
-                    <video className={styles.videoContainer} loop autoPlay controls>
-                        <source src={this.props.video} type="video/webm"></source>
-                        Your browser does not support the video tag.
-                    </video>
-                }
-                {!this.props.video &&
-                    <div className={styles.videoContainer}>
-                        <h2 className={styles.loadingText}>Loading video...</h2>
-                    </div>
-                }
-            </div>
-        )
-
-    }
+  render() {
+    return (
+      <div>
+        {this.props.video && (
+          <video className={styles.videoContainer} loop autoPlay controls>
+            <source src={this.props.video} type="video/webm"></source>
+            Your browser does not support the video tag.
+          </video>
+        )}
+        {!this.props.video && (
+          <div className={styles.videoContainer}>
+            <h2 className={styles.loadingText}>Loading video...</h2>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export { GameVideoRecorder, VideoPlayback };
