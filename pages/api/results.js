e3a742d2bc4f24b664cad15b5e9580c26f788e6e
willob99 <willobrien00@gmail.com>
2020-12-01 21:28:06 -0800

    Add progress to game and results API endpoints.

--- pages/api/results.js
+++ pages/api/results.js
@@ -10,6 +10,8 @@ Requires:
 Returns:
 - Top guessers (guessers)
 - Top tricksters (tricksters)
+- Number of players in game (numPlayers)
+- Number of players that haven't finished guessing (numPlayersRemaining)
 
 */
 
@@ -22,7 +24,7 @@ import { getGame } from "../../src/graphql/queries";
 
 export default async (req, res) => {
 
-    let error = null, waiting = false, gameData, guessers = [], tricksters = [];
+    let error = null, waiting = false, gameData, guessers = [], tricksters = [], numPlayers = 0, numPlayersRemaining = 0;
 
     if (req.query.id) {
 
@@ -35,6 +37,9 @@ export default async (req, res) => {
             ));
 
             if (gameData.data.getGame) {
+                numPlayers = gameData.data.getGame.players.items.length;
+                numPlayersRemaining = numPlayers;
+
                 // check if game is even in session
                 if (gameData.data.getGame.facts.items.length >= gameData.data.getGame.players.items.length) {
                     // check if all connections exist (everyone has submitted answers)
@@ -42,6 +47,12 @@ export default async (req, res) => {
 
                     gameData.data.getGame.previous.items.forEach(previous => {
                         previousConnections += previous.facts.length;
+
+                        // If this player has finished, decrement players remaining.
+                        // Have to count down bc not all players necessarily have an entry in previous.
+                        if (previous.facts.length >= numPlayers - 1) {
+                            numPlayersRemaining--;
+                        }
                     });
 
 
@@ -128,6 +139,8 @@ export default async (req, res) => {
     res.json({ 
         guessers: guessers,
         tricksters: tricksters,
+        numPlayers: numPlayers,
+        numPlayersRemaining: numPlayersRemaining,
         waiting: waiting,
         error: error
     })
