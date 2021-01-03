ea7f4b94e8b886fd8c280e57fc862c305304ddc5
willob99 <willobrien00@gmail.com>
2021-01-03 02:34:56 -0800

    Add API support for refreshing.

--- pages/api/join.js
+++ pages/api/join.js
@@ -7,6 +7,8 @@ Joins a game with a code.
 Requires:
 - Game code (code)
 - Player name (playerName)
+- (optional) Player id (playerId)
+- (optional) Game id (gameId)
 
 Returns:
 - Game ID (gameID)
@@ -23,7 +25,7 @@ import { createPlayer } from "../../src/graphql/custom_mutations";
 import { gameByCode } from "../../src/graphql/custom_queries/joinQueries";
 
 export default async (req, res) => {
-    let gameData, playerData, error = null;
+    let gameData, playerData, playerId, error = null;
 
     try {
         if (req.query.code && req.query.playerName) {
@@ -35,8 +37,15 @@ export default async (req, res) => {
             ));
             
             if (gameData.data.gameByCode.items.length != 0) {
+                // If this user already has a player id that matches a player in this game,
+                // then don't create a new player, just return the same player id (refresh support).
+                if (req.query.playerId !== undefined &&
+                    req.query.gameId !== undefined &&
+                    req.query.gameId === gameData.data.gameByCode.items[0].id &&
+                    gameData.data.gameByCode.items[0].players.items.some((player) => player.id === req.query.playerId)) {
+                        playerId = req.query.playerId;
                 // Accounts for player number + game host doesn't count
-                if (gameData.data.gameByCode.items[0].players.items.length >= gameData.data.gameByCode.items[0].playerNum + 1) {
+                } else if (gameData.data.gameByCode.items[0].players.items.length >= gameData.data.gameByCode.items[0].playerNum + 1) {
                     error = "This game is full.";
                 } else if (gameData.data.gameByCode.items[0].enabled) {
                     // Checks if game is already enabled
@@ -52,6 +61,7 @@ export default async (req, res) => {
                             }
                         }
                     ));
+                    playerId = playerData.data.createPlayer.id;
                 }
             } else {
                 error = "Game not found."
@@ -69,7 +79,7 @@ export default async (req, res) => {
     res.statusCode = 200
     res.json({ 
         gameID: !error ? gameData.data.gameByCode.items[0].id : null,
-        playerID: !error ? playerData.data.createPlayer.id : null,
+        playerID: !error ? playerId : null,
         error: error
     })
 }
