fe7abb02476c0c0d7e72910343043532239e8b26
Daniel Inhee Lee <lee.danny112358@gmail.com>
2021-01-04 20:26:42 -0800

    added mixpanel

--- pages/index.js
+++ pages/index.js
@@ -7,60 +7,59 @@ import Features from "../components/landing/Features";
 import LandingGames from "../components/landing/LandingGames";
 import Contact from "../components/landing/Contact";
 import Footer from "../components/landing/Footer";
+var mixpanel = require("mixpanel-browser");
+mixpanel.init("92c1e92aad6c8ad0239edbd97ceac712");
+mixpanel.track("Site Visit");
 
 export default function Home() {
-    return (
-        <div>
-            <Head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width, initial-scale=1"
-                />
-                <meta name="theme-color" content="#000000" />
-                <meta
-                    name="description"
-                    content="Remote team-building made super simple"
-                />
-                <link rel="apple-touch-icon" href="/images/logo192.png" />
-                <link rel="manifest" href="/manifest.json" />
-                <link rel="icon" href="/favicon.ico" />
-                <title>Covalent</title>
-            </Head>
-            <style jsx global>{`
-                body {
-                    width: 100vw;
-                    height: auto;
-                    overflow-x: hidden;
-                }
-            `}</style>
-            <div className={styles.landingPage}>
-                <NavBar />
-                <div className={styles.navContainer}></div>
-                <span className={styles.pageContent}>
-                    <div id="connect">
-                        <Connect />
-                    </div>
-                    <div id="sharing">
-                        <Sharing />
-                    </div>
-                    <div id="features">
-                        <Features />
-                    </div>
-                    <div id="games">
-                        <LandingGames />
-                    </div>
-                    {/*<div id="contact">
+  return (
+    <div>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <meta
+          name="description"
+          content="Remote team-building made super simple"
+        />
+        <link rel="apple-touch-icon" href="/images/logo192.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <title>Covalent</title>
+      </Head>
+      <style jsx global>{`
+        body {
+          width: 100vw;
+          height: auto;
+          overflow-x: hidden;
+        }
+      `}</style>
+      <div className={styles.landingPage}>
+        <NavBar />
+        <div className={styles.navContainer}></div>
+        <span className={styles.pageContent}>
+          <div id="connect">
+            <Connect />
+          </div>
+          <div id="sharing">
+            <Sharing />
+          </div>
+          <div id="features">
+            <Features />
+          </div>
+          <div id="games">
+            <LandingGames />
+          </div>
+          {/*<div id="contact">
                         <Contact />
             </div>*/}
-                    <div id="footer">
-                        <Footer />
-                    </div>
-                    <div>
-                    </div>
-                </span>
-                {/*<p>The landing page will be here.</p>*/}
-            </div>
-        </div>
-    );
+          <div id="footer">
+            <Footer />
+          </div>
+          <div></div>
+        </span>
+        {/*<p>The landing page will be here.</p>*/}
+      </div>
+    </div>
+  );
 }
