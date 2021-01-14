21818bdedd4afac2ea998cd11c770703d3c5c939
Hritik Arya <hritikarya1402@gmail.com>
2021-01-15 02:25:43 +0530

    Remove Alert
    Alert to be displayed on removed player's page. Redirects to covalent's main page

--- components/RedirectModal.js
+++ components/RedirectModal.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 import styles from "../styles/RedirectModal.module.css";
 
 class RedirectModal extends Component {
-    render () {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick() {
+        window.location = "https://covalent.app/";
+    }
+    render() {
         return (
-            <div className={styles.RedirectModal}>
-                <div className={styles.displayBlock}>
-                <h2 className={styles.displayText}>Sorry, you've been removed from the game.</h2>
+            <div>
+                <div className={styles.modal}>
+                    <div className={styles.modal-content}>
+                        <p>
+                            Sorry, you've been removed from the game. Please
+                            contact the host to rejoin.
+                        </p>
+                        <button className={styles.btn} onClick={this.handleClick}>
+                            Okay
+                        </button>
+                    </div>
                 </div>
             </div>
         );
     }
 }
+
 
 export default RedirectModal;
